25a5ae3c6a3c38dccdb4b497dda30d43c3239089
PUNEET-8 <puneetmakhija4@gmail.com>
2019-11-15 23:42:12 +0530

    for Static files

--- HotelAdmin/settings.py
+++ HotelAdmin/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'HotelAdmin.urls'
@@ -132,4 +133,6 @@ STATICFILES_DIRS = [
 ]
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
